753d0c3188ea7e49e12a2ea94b805c43463e4a26
wukefe <wukefe@gmail.com>
2017-03-24 23:50:35 -0400

    add hotspot c code

---
+++ src/hotspot/c/run.sh
@@ -0,0 +1,8 @@
+./hotspot 512 512 2 4 ../../data/hotspot/temp_512 ../../data/hotspot/power_512
+./hotspot 64 64 2 1 ../../data/hotspot/temp_64 ../../data/hotspot/power_64
+
+
+./hotspot 256 256 20 1 temp_256 power_256
+./hotspot 512 512 20 1 temp_512 power_512
+./hotspot 1024 1024 20 1 temp_1024 power_1024
+./hotspot 2048 2048 20 1 temp_2048 power_2048
