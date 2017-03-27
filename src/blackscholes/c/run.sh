72235e1bd7bee75eba7507fb973b7e3464210012
wukefe <wukefe@gmail.com>
2017-03-27 11:29:24 -0400

    add run script for blackscholes

--- src/blackscholes/c/run.sh
+++ src/blackscholes/c/run.sh
@@ -1,4 +1,22 @@
-./blackscholes 1 in_1M.txt out_1M.txt
-./blackscholes 1 in_2M.txt out_2M.txt
-./blackscholes 1 in_4M.txt out_4M.txt
-./blackscholes 1 in_8M.txt out_8M.txt
+#!/bin/bash
+
+SETSIZE=(1 2 4 8 )
+NUMITER=5
+LOGFILE=run1.log
+
+rm -f $LOGFILE
+
+for k in "${SETSIZE[@]}"
+do
+    echo "echo # Input with in_${k}M.txt >> ${LOGFILE}"
+    echo "echo $NUMITER >> ${LOGFILE}"
+    for i in $(seq 1 $NUMITER)
+    do
+        echo "./blackscholes 1 in_${k}M.txt data/in_${k}M.txt data/out_${k}M.txt grep "elapsed time" | awk -F\: '{print $2}' >> ${LOGFILE}"
+    done
+done
+
+#./blackscholes 1 in_1M.txt out_1M.txt
+#./blackscholes 1 in_2M.txt out_2M.txt
+#./blackscholes 1 in_4M.txt out_4M.txt
+#./blackscholes 1 in_8M.txt out_8M.txt
