82a5b55bee605469c00ce8c2dcde9b619f39f248
wukefe <wukefe@gmail.com>
2017-04-02 02:16:06 -0400

    add test folder

---
+++ test/c/testc.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#benchDirs=( blackscholes hotspot kmeans morgan rprime )
+benchDirs=( rprime )
+
+echo === List of benchmarks ===
+
+count=0
+for each in "${benchDirs[@]}"
+do
+    echo ${count}. $each
+    ((count++))
+done
+
+echo
+
+echo === Building experiments ===
+
+count=0
+for each in "${benchDirs[@]}"
+do
+    echo ${count}. "../../src/$each/c/" | tr -d '\n'
+    make -C "../../src/$each/c/" > /dev/null 2>&1
+    STATUS=${PIPESTATUS[0]}
+    if [ $STATUS -eq 0 ]
+    then
+        echo -n -e " \033[0;32m[pass]\033[0m"
+    else
+        echo -n -e " \033[0;31m[fail]\033[0m"
+    fi
+    echo 
+    ((count++))
+done
+
+echo === Running experiments ===
+
+count=0
+for each in "${benchDirs[@]}"
+do
+    echo ${count}. "../../src/$each/c/"
+    (cd ../../src/$each/c && ./run.sh)
+    STATUS=${PIPESTATUS[0]}
+    if [ $STATUS -eq 0 ]
+    then
+        echo -e "\033[0;32m[pass]\033[0m"
+    else
+        echo -e "\033[0;31m[fail]\033[0m"
+    fi
+    LOGFILE=result_${each}.log
+    #echo " Copying $LOGFILE to current directory"
+    cp ../../src/$each/c/$LOGFILE $LOGFILE
+    echo "Result [$each]:"
+    ../calculate $LOGFILE
+    ((count++))
+done
+
+
